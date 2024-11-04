2d50528afc2a33c1804cb366a6ef325cf62067d3
natashandrigoz <nata_shandrigoz@mail.ru>
2024-11-04 20:13:27 +0200

    add task
    задачи со строками

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Task 1. Составить программу, которая:
+			//а) запрашивает имя человека и повторяет его на экране;
+			//б) запрашивает имя человека и повторяет его на экране с приветствием.
+			//Console.Write("Введите Ваше имя:  ");
+			//string name = Console.ReadLine();
+			//Console.Write(name);
+			//Console.Write("Введите Ваше имя:  ");
+			//string name = Console.ReadLine();
+			//Console.Write($"Здравствуйте, {name}!");
+
+			//Task 2. Составить программу, которая запрашивает
+			//название футбольной команды и повторяет его
+			//на экране со словами "… — это чемпион!".
+			//Console.Write("Название футбольной команды: ");
+			//string name = Console.ReadLine();
+			//Console.WriteLine($"{name} — это чемпион!");
+
+			//Task 3. Составить программу, которая запрашивает отдельно имя и отдельно фамилию,
+			//а затем выводит их как одну символьную строку.
+			//Console.Write("Введите Ваше имя:  ");
+			//string name = Console.ReadLine();
+			//Console.Write("Введите Вашу фамилию:  ");
+			//string surname = Console.ReadLine();
+			//Console.Write($"{name} {surname}");
+
+			//Task 4. Составить программу, которая запрашивает название государства и его столицы,
+			//а затем выводит сообщение: "Столица государства ... — город ..."
+			//(на месте многоточий должны быть выведены соответствующие значения).
+			//Console.Write("Введите название государства: ");
+			//string state= Console.ReadLine();
+			//Console.Write("Введите название столицы государства: ");
+			//string capital = Console.ReadLine();
+			//Console.WriteLine($"Столица государства {state} — город {capital}");
+
+			//Task 5. Составить программу, которая запрашивает название романа и фамилию его автора,
+			//а затем выводит сообщение: "Писатель ... — автор романа ..."
+			//(на месте многоточий должны быть выведены соответствующие значения).
+			//Console.Write("Введите название романа: ");
+			//string title = Console.ReadLine();
+			//Console.Write("Введите фамилию автора романа: ");
+			//string surname = Console.ReadLine();
+			//Console.WriteLine($"Писатель {surname} — автор романа {title}");
+
+			//Task 6. Дано название футбольного клуба. Определить количество символов в нем.
+			//Console.Write("Введите название футбольного клуба: ");
+			//string name=Console.ReadLine();
+			//Console.WriteLine($"Количество символов в названии: {name.Length}");
+
+			//Task 7.Напишите программный код к следующему диалогу:
+			//Ты вчера был болен. 
+			//Измерь - ка температуру!
+			//Сообщи, какая у тебя температура: 36.5
+			//Ты здоров, дружок!
+			//Можешь идти в школу.
+			//Желаю успехов!
+			//Console.WriteLine("Ты вчера был болен.");
+			//Console.WriteLine("Измерь-ка температуру!");
+			//Console.Write("Сообщи, какая у тебя температура: ");
+			//double temp=double.Parse(Console.ReadLine());
+			//Console.WriteLine("Ты здоров, дружок!");
+			//Console.WriteLine("Можешь идти в школу.");
+			//Console.WriteLine("Желаю успехов!");
+
+			//Task 8. Напишите программный код к следующему диалогу:
+			//Здравствуй, я компьютер, а тебя как зовут? Коля
+			//Очень приятно, Коля. 
+			//Сколько тебе лет? 16
+			//Ого! Целых 16 лет! Ты уже совсем взрослый!
+			//Console.Write("Здравствуй, я компьютер, а тебя как зовут? ");
+			//string name = Console.ReadLine();
+			//Console.WriteLine($"Очень приятно, {name}");
+			//Console.Write("Сколько тебе лет? ");
+			//int age=int.Parse(Console.ReadLine());
+			//Console.WriteLine($"Ого! Целых {age} лет! Ты уже совсем взрослый!");
+
+			//Task 9.Напишите программный код к следующему диалогу:
+			//Как тебя зовут? Дима
+			//Привет, Дима!
+			//Сколько тебе лет? 17
+			//Какой у тебя любимый предмет в техникуме? история
+			//Какая у тебя любимая книга? Петр I
+			//Значит тебя зовут, Дима.
+			//Тебе 17 лет.
+			//Твой любимый предмет - история.
+			//И твоя любимая книга – Петр I
+			//Console.Write("Как тебя зовут? ");
+			//string name = Console.ReadLine();
+			//Console.WriteLine($"Привет, {name}");
+			//Console.Write($"Сколько тебе лет? ");
+			//int age=int.Parse( Console.ReadLine() );
+			//Console.Write("Какой у тебя любимый предмет в техникуме? ");
+			//string subject=Console.ReadLine();
+			//Console.Write("Какая у тебя любимая книга? ");
+			//string book = Console.ReadLine();
+			//Console.WriteLine($"Значит тебя зовут, {name}");
+			//Console.WriteLine($"Тебе {age} лет.");
+			//Console.WriteLine($"Твой любимый предмет - {subject}");
+			//Console.WriteLine($"И твоя любимая книга – {book}");
+			Console.Read();
+		}
+	}
+}
